7514b1548111290c4fed36c426418cdd41f2cd2d
ji980207 <86302014+shji0318@users.noreply.github.com>
2022-09-17 18:10:20 +0900

    Update SaveData.cs

--- Scripts/SaveData.cs
+++ Scripts/SaveData.cs
@@ -6,17 +6,11 @@ using System;
 [Serializable]
 public class SaveData
 {
-    // SceneName
-    // player => dungeonlevel hasWeapons[], hasWeaponValue, heart, money inventory(items)
-    //
-
     public Vector3 _position;
     public int _dungeonLevel;
     public bool[] _hasWeapons;
     public int[] _hasWeaponValue;
     public int _heart;
     public int _money;
-    public List<InvenItem> _items;
-    
-    
+    public List<InvenItem> _items;   
 }
